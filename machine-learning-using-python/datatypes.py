d67a622eceebb101b6736cfdcbda4bdd42e1fe6a
VarunAravinth <varunaravinh28@gmail.com>
2018-03-26 20:46:17 -0700

    day1 python added

---
+++ machine-learning-using-python/datatypes.py
@@ -0,0 +1,122 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('hello world')
+hello world
+>>> print()
+()
+>>> print('')
+
+>>> print 'hhii'
+hhii
+>>> print 'hiii' #wont work in python 3_*
+hiii
+>>> #wnjrwbgwr wgrwngrlwjgrikwhiii
+>>> #variables
+>>> #statically  typed || dynamicallly typed language
+>>> a=8
+>>> a='fjwgjwb'
+>>> print a
+fjwgjwb
+>>> ss = 10
+>>> print = 1-
+SyntaxError: invalid syntax
+>>> print = 10
+SyntaxError: invalid syntax
+>>> ss = 10
+>>> type(ss)
+<type 'int'>
+>>> ss = 'jikkk'
+>>> a
+'fjwgjwb'
+>>> type(ss)
+<type 'str'>
+>>> ss=1.0
+>>> type(ss)
+<type 'float'>
+>>> ss= true
+
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    ss= true
+NameError: name 'true' is not defined
+>>> ss
+1.0
+>>> true = 10
+>>> ss = true
+>>> type(ss)
+<type 'int'>
+>>> ss= 'true'
+>>> #green - string , orange or pink - inbuilt keywords, white - userdefined stuff
+>>> ss = true
+>>> ss = True
+>>> type(ss)
+<type 'bool'>
+>>> ss = false
+
+Traceback (most recent call last):
+  File "<pyshell#31>", line 1, in <module>
+    ss = false
+NameError: name 'false' is not defined
+>>> ss = false
+
+Traceback (most recent call last):
+  File "<pyshell#32>", line 1, in <module>
+    ss = false
+NameError: name 'false' is not defined
+>>> ss = False
+>>> type(ss)
+<type 'bool'>
+>>> false = 10
+>>> ab = 10
+>>> ab
+10
+>>> print ab
+10
+>>> print(ab)
+10
+>>> print('ab')
+ab
+>>> ab
+10
+>>> cd='20'
+>>> type(ab);type(cd)
+<type 'int'>
+<type 'str'>
+>>> ab + cd
+
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    ab + cd
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> ab + int(cd)
+30
+>>> str(ab) + cd
+'1020'
+>>> de = 'strtrt'
+>>> type(cd)
+<type 'str'>
+>>> cd + de
+'20strtrt'
+>>> int(de)
+
+Traceback (most recent call last):
+  File "<pyshell#50>", line 1, in <module>
+    int(de)
+ValueError: invalid literal for int() with base 10: 'strtrt'
+>>> 10 + 10
+20
+>>> 'hi ' + 'hello'
+'hi hello'
+>>> 10 + 20 + 'hiii'
+
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    10 + 20 + 'hiii'
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> 'jfef' + 10 + 20
+
+Traceback (most recent call last):
+  File "<pyshell#54>", line 1, in <module>
+    'jfef' + 10 + 20
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> 
